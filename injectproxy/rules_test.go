0fa17e4e1ea0c3a95b826eabf36636950e300a83
Jonas Schäfer <j.wielicki@sotecware.net>
2020-12-15 08:52:15 +0100

    Add option to get label value from an HTTP header
    This allows to use the proxy in settings where query parameters
    cannot be used; this could be behind specific reverse proxies, or
    when HTTP POST is required (see #53).
    
    This work is heavily based on the work done by
    Andreas Plesner <apj@mutt.dk> in [1]. I (Jonas Schäfer) ported
    this to the current head of master.
    
       [1]: https://github.com/prometheus-community/prom-label-proxy/pull/54
    
    Signed-off-by: Jonas Schäfer <j.wielicki@sotecware.net>

--- injectproxy/rules_test.go
+++ injectproxy/rules_test.go
@@ -658,7 +658,7 @@ func TestRules(t *testing.T) {
 		t.Run(fmt.Sprintf("%s=%s", proxyLabel, tc.labelv), func(t *testing.T) {
 			m := newMockUpstream(tc.upstream)
 			defer m.Close()
-			r, err := NewRoutes(m.url, proxyLabel)
+			r, err := NewRoutes(m.url, proxyLabel, WithValueFromQuery(proxyLabel))
 			if err != nil {
 				t.Fatalf("unexpected error: %v", err)
 			}
--- main.go
+++ main.go
@@ -32,14 +32,19 @@ func main() {
 		insecureListenAddress  string
 		upstream               string
 		label                  string
+		headerName             string
+		queryParam             string
 		enableLabelAPIs        bool
 		unsafePassthroughPaths string // Comma-delimited string.
 		errorOnReplace         bool
+		header                 string
 	)
 
 	flagset := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
 	flagset.StringVar(&insecureListenAddress, "insecure-listen-address", "", "The address the prom-label-proxy HTTP server should listen on.")
 	flagset.StringVar(&upstream, "upstream", "", "The upstream URL to proxy to.")
+	flagset.StringVar(&queryParam, "query-param", "", "The query parameter to obtain the label value from. This or -header is required.")
+	flagset.StringVar(&headerName, "header", "", "The HTTP header name to obtain the label value from. This or -query-param is required.")
 	flagset.StringVar(&label, "label", "", "The label to enforce in all proxied PromQL queries. "+
 		"This label will be also required as the URL parameter to get the value to be injected. For example: -label=tenant will"+
 		" make it required for this proxy to have URL in form of: <URL>?tenant=abc&other_params...")
@@ -57,6 +62,18 @@ func main() {
 		log.Fatalf("-label flag cannot be empty")
 	}
 
+	var opts []injectproxy.Option
+
+	if (queryParam != "" && header != "") || (queryParam == "" && header == "") {
+		log.Fatal("exactly one of -query-param and -header must be given")
+	}
+	if queryParam != "" {
+		opts = append(opts, injectproxy.WithValueFromQuery(queryParam))
+	}
+	if header != "" {
+		opts = append(opts, injectproxy.WithValueFromHeader(header))
+	}
+
 	upstreamURL, err := url.Parse(upstream)
 	if err != nil {
 		log.Fatalf("Failed to build parse upstream URL: %v", err)
@@ -66,7 +83,6 @@ func main() {
 		log.Fatalf("Invalid scheme for upstream URL %q, only 'http' and 'https' are supported", upstream)
 	}
 
-	var opts []injectproxy.Option
 	if enableLabelAPIs {
 		opts = append(opts, injectproxy.WithEnabledLabelsAPI())
 	}
